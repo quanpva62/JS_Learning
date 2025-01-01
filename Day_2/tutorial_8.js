7fff6521ca752d22125b4e05dd2acc8b6e057dba
quanpva62 <phamquan6023@gmail.com>
2025-01-02 01:57:38 +0700

    Day 2 part 1

---
+++ Day_2/tutorial_8.js
@@ -0,0 +1,18 @@
+//let result = condition ? value1 : value2;
+
+// Task 1
+// let result =  prompt("What is the official name of JavaScript?");
+// result == "ECMAScript" ? alert("Right!") : alert("You don't know? ECMAScript!");
+
+//Task 2
+// let result = prompt("Enter a number");
+// result == 0 ? alert(0) : result > 0 ? alert(1) : alert(-1);
+
+// //Task 3
+// let result;
+// result == a + b < 4 ? 'Below' : 'Over';
+
+//Task 4
+let message;
+let login = prompt("Enter your login");
+message = login == "Employee" ? "Helo" : "Director" ? "Greetings" : "" ? "" : "";
